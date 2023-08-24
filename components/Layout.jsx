82c4fd37dee047c238ec73e8194f86d0051d99b5
fkmanny <fakoyaibk@gmail.com>
2023-08-24 01:11:34 +0100

    bug

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -9,7 +9,7 @@ const Layout = ({ children }) => {
   return (
     <div className="layout">
       <Head>
-        <title>JS Mastery Store</title>
+        <title>E Store</title>
       </Head>
       <header>
         <Header />
