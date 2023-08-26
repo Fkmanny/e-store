ca7eb18a3b5569d14cc9a46b8aefc1f7bda21e43
fkmanny <fakoyaibk@gmail.com>
2023-08-26 13:18:33 +0100

    Responsive build

--- pages/canceled.jsx
+++ pages/canceled.jsx
@@ -1,15 +1,16 @@
 import { useRouter } from 'next/router'
 import React, { useEffect } from 'react'
 
-const cancelled = () => {
-    const router = useRouter();
-    useEffect(() => {
-        // Redirect logic here
-        router.push('/');
-      }, []);
+const Canceled = () => {
+  const router = useRouter();
+  useEffect(() => {
+      // Redirect logic here
+      router.push('/');
+  }, []);
+  
   return (
-    <></>
+      <div></div>
   )
 }
 
-export default cancelled
+export default Canceled;
